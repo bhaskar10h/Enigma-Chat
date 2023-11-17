b4ca7044a604ed36c0ee8a7c76b55202852acf99
bhaskar10h <112790780+bhaskar10h@users.noreply.github.com>
2023-11-17 22:18:31 +0530

    Update client.py

--- Terminal-Chat/client.py
+++ Terminal-Chat/client.py
@@ -3,26 +3,31 @@ import threading
 
 def receive_messages(client_socket):
     while True:
-        # Receive and print messages from the server
-        data = client_socket.recv(1024)
-        print(f"Received from server: {data.decode('utf-8')}")
+        try:
+            # Receive and print messages from the server
+            data = client_socket.recv(1024)
+            print(f"Received from server: {data.decode('utf-8')}")
+        except ConnectionResetError:
+            print("Connection with the server was reset.")
+            break
 
 # Define the server address (host and port)
-host = '127.0.0.1'
-port = 12345
+ip_addr = input("Enter the your ip_addr : ") # Enter the server IP-Address
+server_host = ip_addr  # Replace with the actual IP address or hostname of the server
+server_port = 12345
 
 # Create a socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect to the server
-client_socket.connect((host, port))
-print(f"Connected to {host}:{port}")
-
-# Start a thread to receive messages from the server
-receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
-receive_thread.start()
-
 try:
+    # Connect to the server
+    client_socket.connect((server_host, server_port))
+    print(f"Connected to {server_host}:{server_port}")
+
+    # Start a thread to receive messages from the server
+    receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+    receive_thread.start()
+
     while True:
         # Get user input and send it to the server
         message = input("Enter a message to send to the server (type 'exit' to quit): ")
@@ -32,8 +37,11 @@ try:
         
         client_socket.sendall(message.encode('utf-8'))
 
-except KeyboardInterrupt:
-    print("Client interrupted.")
+except ConnectionRefusedError:
+    print(f"Connection to {server_host}:{server_port} was refused. Check the server status and network settings.")
+
+except Exception as e:
+    print(f"An error occurred: {e}")
 
 finally:
     # Close the socket
