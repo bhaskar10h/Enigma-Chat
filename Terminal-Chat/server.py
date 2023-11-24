29a8da891ddd91df770c9110d894e14faef1dfc7
bhaskar10h <112790780+bhaskar10h@users.noreply.github.com>
2023-11-24 21:06:24 +0530

    Update server.py

--- Terminal-Chat/server.py
+++ Terminal-Chat/server.py
@@ -17,9 +17,22 @@ print(f"Server listening on {host}:{port}")
 
 # List to store client sockets
 client_sockets = []
+client_nicknames = {}
+online_clients = set()
 
 def handle_client(client_socket, client_address):
     try:
+        # Get the nickname from the client
+        nickname_data = client_socket.recv(1024)
+        nickname = nickname_data.decode().strip()
+        client_nicknames[client_socket] = nickname
+        online_clients.add(client_socket)
+
+        # Broadcast a notification when a client connects
+        for other_client_socket in client_sockets:
+            if other_client_socket != client_socket:
+                other_client_socket.sendall(f"{nickname} has joined.".encode())
+
         while True:
             data = client_socket.recv(1024)
             if not data:
@@ -28,22 +41,31 @@ def handle_client(client_socket, client_address):
             decoded_data = data.decode()
             if decoded_data.startswith("/list_clients"):
                 # Send a list of connected clients to the requesting client
-                client_socket.sendall(str(client_sockets).encode())
+                client_socket.sendall(str(client_nicknames.values()).encode())
             elif decoded_data.startswith("/server_broadcast"):
                 # Broadcast a server message to all clients
                 message = "Server: " + decoded_data[len("/server_broadcast"):].strip()
                 for other_client_socket in client_sockets:
                     if other_client_socket != client_socket:
                         other_client_socket.sendall(message.encode())
+            elif decoded_data.startswith("/private"):
+                # Extract the target username and message
+                _, target_username, message = decoded_data.split(" ", 2)
+                target_client = next((sock for sock, nick in client_nicknames.items() if nick == target_username), None)
+                if target_client:
+                    target_client.sendall(f"Private message from {nickname}: {message}".encode())
+                else:
+                    client_socket.sendall("User not found.".encode())
             else:
                 # Broadcast the message to all other clients with sender's address
-                message = f"{client_address}: {decoded_data}"
+                message = f"{nickname}: {decoded_data}"
                 for other_client_socket in client_sockets:
                     if other_client_socket != client_socket:
                         other_client_socket.sendall(message.encode())
     except Exception as e:
         print(f"Client {client_address} disconnected: {e}")
     finally:
+        online_clients.remove(client_socket)
         client_sockets.remove(client_socket)
         client_socket.close()
 
@@ -54,6 +76,9 @@ while True:
 
     # Add the new client socket to the list
     client_sockets.append(client_socket)
+
+    # Send a welcome message and request nickname from the client
+    client_socket.sendall("Welcome to the chat server! Please set your nickname using /set_nickname <your_nickname>".encode())
 
     # Start a new thread to handle the client
     client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
